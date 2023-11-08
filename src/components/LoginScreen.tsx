ade3e78c8a6988ce1796419a27f7f8b1c03dc9b2
Leo Wang <ab0988956087@gmail.com>
2023-11-08 11:10:59 +0800

    feat(login-screen): finish styling

--- src/components/LoginScreen.tsx
+++ src/components/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 import type { RootStackParamList } from '../../App'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
@@ -7,12 +7,53 @@ interface LoginProps extends NativeStackScreenProps<RootStackParamList,'Login'>{
 }
 
 const LoginScreen = ({navigation}:LoginProps) => {
+
+  const login = () => {
+    // TODO
+    console.log("TODO: login")
+   }
+
   return (
-    <View>
-      <Text>Login</Text>
-      <Button title='Sign Up' onPress={() => { navigation.navigate('SignUp') }}/>
+    <View style={styles.screen}>
+      <Text style={styles.heading}>Login to Message Man</Text>
+      <TextInput style={styles.input} placeholder='Username'/>
+      <TextInput style={styles.input} placeholder='Password'secureTextEntry/>
+      <Button title='    LOGIN    ' onPress={login}/>
+      <Text style={styles.text}>
+        Don't have an account?
+        <Text style={styles.link} onPress={() => { navigation.navigate('SignUp') }}>{` Sign Up`}</Text>
+      </Text>
     </View>
     )
 }
+
+const styles=StyleSheet.create({
+  screen:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  heading:{
+    fontSize:30,
+    textAlign:'center',
+    marginBottom:10
+  },
+  text:{
+    textAlign:'center',
+    marginTop:10
+  },
+  link:{
+    color:'blue',
+    fontWeight:'bold'
+  },
+  input:{
+    borderWidth:1,
+    padding:10,
+    margin:10,
+    width:300,
+    fontSize:16
+  }
+})
+
 
 export default LoginScreen
