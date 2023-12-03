fea338e1758b6be60c180d7a5a53a7dc5025fbf3
Leo Wang <ab0988956087@gmail.com>
2023-12-03 11:17:59 +0800

    feat: store user login state and auto navigate

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/components/LoginScreen';
 import SignUpScreen from './src/components/SignUpScreen';
 import OverviewScreen from './src/components/OverviewScreen';
+import { User, getAuth } from 'firebase/auth';
+import useUserStore from './src/stores/userStore';
+import { useEffect } from 'react';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -13,12 +16,38 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const { user, setUser } = useUserStore();
+
+
+  const onAuthStateChanged = (user: User | null) => {
+    if (user) {
+      setUser(user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    const subscriber = getAuth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Overview" component={OverviewScreen} />
+        {user ?
+          (
+            <>
+              <Stack.Screen name="Overview" component={OverviewScreen} />
+            </>
+          ) :
+          (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="SignUp" component={SignUpScreen} />
+            </>
+          )
+        }
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/components/OverviewScreen.tsx
+++ src/components/OverviewScreen.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../../App'
 import useUserStore from '../stores/userStore'
+import { getAuth, signOut } from 'firebase/auth'
 
 interface OverviewProps extends NativeStackScreenProps<RootStackParamList, 'Overview'> {
 
@@ -10,10 +11,20 @@ interface OverviewProps extends NativeStackScreenProps<RootStackParamList, 'Over
 
 
 const OverviewScreen = ({ navigation }: OverviewProps) => {
-  const { user } = useUserStore();
+  const { user, setUser } = useUserStore();
+  const onPressSignOut = async () => {
+    try {
+      await signOut(getAuth());
+      setUser(null);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <View>
       <Text>{JSON.stringify(user)}</Text>
+      <Button title='    LOGOUT   ' onPress={onPressSignOut} />
     </View>
   )
 }
